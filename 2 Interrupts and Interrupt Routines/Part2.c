a813538376a9d0ab34bb7d9e899a77ee5a8ad8bc
gianna brock <brockg95@students.rowan.edu>
2023-01-31 17:31:49 -0500

    it blinks the lights
    made the if statement do the thing

--- 2 Interrupts and Interrupt Routines/Part2.c
+++ 2 Interrupts and Interrupt Routines/Part2.c
@@ -27,7 +27,9 @@ int main(void)
     P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
     P1DIR |= BIT0;                          // Set P1.0 to output direction
 
-    // @TODO You need to add in the configuration for the Green LED
+    // check @TODO You need to add in the configuration for the Green LED
+    P6OUT &= ~BIT6;                         // Clear P6.6 output latch for a defined power-on state
+    P6DIR |= BIT6;                          // Set P6.6 to output direction
 
     P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
     P2REN |= BIT3;                          // P2.3 pull-up register enable
@@ -40,22 +42,24 @@ int main(void)
 
     P2IFG &= ~BIT3;                         // P2.3 IFG cleared
 
-    __bis_SR_register(GIE);                 // Enter LPM3 w/interrupt
+    __bis_SR_register(GIE);                 // Enter LPM3 w/interrupt MUST BE THERE to interpret interrupts
 
     while(1)
     {
         // @TODO You will need to modify this code to change between blinking the Red LED or the Green LED
         if (ToggleEnable)
-            P1OUT ^= BIT0;                  // P1.0 = toggle
+        { P1OUT ^= BIT0;                  // P1.0 = toggle
+           P6OUT = ~BIT6;}                // P6.6 is off
         else
-            P1OUT &= ~BIT0;                 // Set P1.0 to 0
+        {  P1OUT = ~BIT0;                 // Set P1.0 to 0
+           P6OUT ^= BIT6;}                // P6.6 = toggle but in an if statement way
         __delay_cycles(100000);
     }
 }
 
 // Port 2 interrupt service routine
 #pragma vector=PORT2_VECTOR
-__interrupt void Port_2(void)
+__interrupt void Port_2(void) //little as possible code during an interrupt
 {
     // @TODO You might need to modify this based on your approach to the lab
     P2IFG &= ~BIT3;                         // Clear P1.3 IFG
